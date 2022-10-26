2b3d2d894f47405d81d05b5e17578392df185f93
Ahmad J. Bright <ajbright213@gmail.comclear>
2022-10-26 18:22:26 -0400

    Completed Challenge 12

--- library.py
+++ library.py
@@ -1,23 +1,40 @@
+from turtle import title
 from book import Book
 
 
 class Library():
     def __init__(self):
         """Initialize the empty book list"""
-        pass
+        self.books = []
 
-    def add_title(self):
+    def add_title(self, title, author):
         """Add a Book object with the given title and author to the book list"""
-        pass
+ 
+        self.books.append(Book(title, author))
 
     def count_books(self):
+        return len(self.books)
         """Return the number of books currently in the booklist"""
-        pass
+        
 
-    def remove_title(self):
+
+
+    def remove_title(self, title):
+        count = 0
+        for item in self.books:
+            if item.title == title:
+                self.books.pop(count)
+            count += 1
+
         """Remove a book from the book list"""
-        pass
+        
+        
 
     def is_empty(self):
         """Return True if the book list is empty, False if not"""
         return self.books == []
+
+
+    def display_books(self):
+        for book in self.books:
+            print(book)
